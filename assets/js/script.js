7d470bd681b866421eef634e10a10b70c3e55543
megha875 <meghaagarwal2436@gmail.com>
2024-05-30 21:45:45 +0530

    code

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var marks =86;
+    
+if(marks>=80 && m<=100)
+{
+    document.write("You are 1st class.");
+
+}
+else if(marks>=60 && marks<=79)
+{
+    document.write("You are 2nd class.");
+}
+else if(marks>=40 && marks<=59)
+{
+    document.write("You are 3rd class.");
+}
+else
+{
+    document.write("You are fail.");
+}
